22f4fe243329bfa637eea7089477f6b0cd36ac9d
soasis3 <soasis3@gmail.com>
2026-05-07 19:17:26 +0900

    Keep AnimOut controls inside scene browser

--- rrAnimout.py
+++ rrAnimout.py
@@ -4038,11 +4038,19 @@ def rrAnimout_UI():
     cmds.frameLayout(lv=0, w=302, mh=0, mw=0, backgroundColor=[0.26, 0.26, 0.26])
     cmds.columnLayout(adjustableColumn=False, backgroundColor=[0.26, 0.26, 0.26], co=('both', 0), rs=0)
     cmds.text(label="SF ANIMOUT_test", align='center', height=24, enableBackground=False)
+    cmds.setParent('..')
+    # SCENE BROWSER
+    cmds.frameLayout(cll=1, lv=1, l='SCENE BROWSER', fn="smallPlainLabelFont", mh=0, mw=8, backgroundColor=[0.26, 0.26, 0.26])
+    cmds.columnLayout(adjustableColumn=False, backgroundColor=[0.29, 0.29, 0.29], co=('both', 3), rs=3)
+    cmds.separator(height=1, style='none')
+
     if can_show_deploy_tools():
-        cmds.rowLayout(numberOfColumns=2, columnWidth2=[151, 151], columnAlign=[(1, 'center'), (2, 'center')])
-        cmds.button(label="Reload", height=24, width=151, backgroundColor=[0.32, 0.36, 0.36], command=reload_rranimout)
-        cmds.button(label="Deploy", height=24, width=151, backgroundColor=[0.36, 0.32, 0.32], command=deploy_rranimout)
+        cmds.rowLayout(numberOfColumns=2, columnWidth2=[138, 138], columnAlign=[(1, 'center'), (2, 'center')])
+        cmds.button(label="Reload", height=24, width=138, backgroundColor=[0.32, 0.36, 0.36], command=reload_rranimout)
+        cmds.button(label="Deploy", height=24, width=138, backgroundColor=[0.36, 0.32, 0.32], command=deploy_rranimout)
         cmds.setParent('..')
+
+    cmds.rowLayout(numberOfColumns=1, columnWidth1=276, columnAlign=[(1, 'center')])
     global projectMenuName
     current_project = get_current_project()
     set_current_project(current_project)
@@ -4051,12 +4059,7 @@ def rrAnimout_UI():
         cmds.menuItem(label=project_name)
     cmds.optionMenu(projectMenuName, edit=True, value=current_project, changeCommand=update_scenes)
     cmds.setParent('..')
-    cmds.setParent('..')
 
-    # SCENE BROWSER
-    cmds.frameLayout(cll=1, lv=1, l='SCENE BROWSER', fn="smallPlainLabelFont", mh=0, mw=8, backgroundColor=[0.26, 0.26, 0.26])
-    cmds.columnLayout(adjustableColumn=False, backgroundColor=[0.29, 0.29, 0.29], co=('both', 3), rs=3)
-    cmds.separator(height=1, style='none')
 
     cmds.rowLayout(numberOfColumns=3, columnWidth3=[90, 91, 91], columnAlign=[(1, 'center'), (2, 'center'), (3, 'center')])
     cmds.text(label="SCENE", height=20, width=90)
